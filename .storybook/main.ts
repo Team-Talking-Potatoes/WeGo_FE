6fc63865364fff0b78492c2ecf458f2be961810a
namgungjongmin <jmnamgung@gmail.com>
2024-12-02 16:11:52 +0900

    conf: 각종 설정파일 수정 및 파일 정리
    - tailwind.config 파일 컨텐트 추가
    - @types 폴더 .gitkeep 삭제
    - 스토리북 main.ts 에드온 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,13 +3,24 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
   staticDirs: ['..\\public'],
 };
