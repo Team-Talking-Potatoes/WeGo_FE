411f8cd0cd46340bb0116f9661431f25bfb84088
namgungjongmin <jmnamgung@gmail.com>
2024-12-03 15:11:29 +0900

    conf: tailwindcss 프로젝트 색상 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,42 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: {
+          normal: '#2563EB',
+          white: '#FFFFFF',
+        },
+
+        label: {
+          normal: '#222222',
+          strong: '#000000',
+          neutral: '#46474C',
+          alternative: '#878A92',
+          assistive: '#C2C4C7',
+          disable: '#DBDCDF',
+        },
+
+        background: {
+          normal: '#FFFFFF',
+          alternative: '#F7F7F8',
+        },
+
+        interaction: {
+          inactive: '#989BA1',
+          disable: '#F7F7F8',
+        },
+
+        line: {
+          normal: '#E0E0E2',
+          strong: '#222222',
+          neutral: '#E8E8EA',
+          alternative: '#F4F4F5',
+        },
+
+        status: {
+          success: '#52BB65',
+          error: '#EC524B',
+          infomative: '#4A8AF8',
+        },
       },
       fontFamily: {
         pretendard: ['var(--font-pretendard)', 'sans-serif'],
