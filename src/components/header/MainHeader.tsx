277a127bb2656ea77fe1501381cf235c551ca478
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-04 13:48:14 +0900

    refactor: header refactoring - 스크롤 이벤트에 throttle 적용 - clsx 적용으로 중복 스타일 제거 - 아이콘마다 aria-label 적용

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -4,41 +4,44 @@ import Link from 'next/link';
 import Logo from '@/assets/logo.svg';
 import Mypage from '@/assets/mypage.svg';
 import Write from '@/assets/write.svg';
+import { throttle } from 'lodash';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import clsx from 'clsx';
 
 const MainHeader = () => {
   const pathname = usePathname();
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll = throttle(() => {
       if (window.scrollY > 280) {
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
       }
-    };
+    }, 300);
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [isScrolled]);
+  }, []);
 
   if (pathname === '/') {
     return (
       <header
-        className={`fixed flex h-[60px] w-full items-center justify-between px-5 py-[14px] transition-all duration-300 ease-in-out ${
+        className={clsx(
+          'fixed z-20 flex h-[60px] w-full items-center justify-between px-5 py-[14px] transition-all duration-300 ease-in-out',
           isScrolled
-            ? 'bg-white bg-opacity-70 bg-gradient-to-b from-transparent to-white backdrop-blur-lg'
-            : 'bg-transparent'
-        }`}
+            ? 'bg-gradient-to-b from-transparent to-white backdrop-blur-lg'
+            : '',
+        )}
       >
         <Link href="/">
-          <div>{isScrolled}</div>
           <Logo
             width={80}
             height={32}
-            className={isScrolled ? 'text-[#222222]' : 'text-[#ffffff]'}
+            className={isScrolled ? 'text-label-normal' : 'text-primary-white'}
+            aria-label="WEGO 로고"
           />
         </Link>
         <div className="flex items-center gap-3">
@@ -46,14 +49,20 @@ const MainHeader = () => {
             <Write
               width={24}
               height={24}
-              className={isScrolled ? 'text-[#222222]' : 'text-[#ffffff]'}
+              className={
+                isScrolled ? 'text-label-normal' : 'text-primary-white'
+              }
+              aria-label="모임 만들기"
             />
           </Link>
           <Link href="/">
             <Mypage
               width={24}
               height={24}
-              className={isScrolled ? 'text-[#222222]' : 'text-[#ffffff]'}
+              className={
+                isScrolled ? 'text-label-normal' : 'text-primary-white'
+              }
+              aria-label="마이페이지로 가기"
             />
           </Link>
         </div>
