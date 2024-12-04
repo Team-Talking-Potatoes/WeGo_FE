0cdec0c87907db87ac454307b078bd4127201d20
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-04 14:54:40 +0900

    refactor: 시맨틱 태그로 교체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import reviewList from '@/mocks/review/reviewList.json';
 import userList from '@/mocks/user/popularUser.json';
 import Userpopular from '@/components/main/Userpopular';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="">
+    <main>
       <HeroSection />
       <WeeklyPopular travelList={travelList} />
       <ReviewListSection reviewList={reviewList} />
       <Userpopular userList={userList} />
-    </div>
+    </main>
   );
-}
+};
+export default Home;
