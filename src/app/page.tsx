d5863acfbb2c647dedd5798bdc5207954bd3645f
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-04 10:50:21 +0900

    feat: main 여행지기 section -user card 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import WeeklyPopular from '@/components/main/WeeklyPopular';
 import travelList from '@/mocks/travel/travelList.json';
 import ReviewListSection from '@/components/main/ReviewListSection';
 import reviewList from '@/mocks/review/reviewList.json';
+import userList from '@/mocks/user/popularUser.json';
+import Userpopular from '@/components/main/Userpopular';
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <WeeklyPopular travelList={travelList} />
       <ReviewListSection reviewList={reviewList} />
+      <Userpopular userList={userList} />
     </div>
   );
 }
