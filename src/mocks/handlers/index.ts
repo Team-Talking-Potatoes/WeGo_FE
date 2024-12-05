243b77da6009f0b67513e6f159c582e1640e93c2
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-05 18:37:10 +0900

    test: 임시 설정

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import template from './template';
 import login from './login';
 import signup from './signup';
+import popularTravel from './travel/popularTravel';
 
-export const handlers = [...template, login, ...signup];
+export const handlers = [...template, login, ...signup, popularTravel];
