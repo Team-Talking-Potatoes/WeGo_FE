464c8cfac3ba2f829900a0497cdb8ae777c11adf
who0803 <10rlatkdtn@naver.com>
2024-12-06 11:14:48 +0900

    feat: input 컴포넌트 기능 추가 및 변경
    - TextInput 및 TextInputWithLabel 컴포넌트 reaOnly 추가
    - TextInputWithLabel 및 TextareaWithLabel 컴포넌트 inputClassname
      추가하여 인풋에도 스타일 커스텀할 수 있도록 적용

--- src/components/inputwithlabel/TextInputWithLabel.tsx
+++ src/components/inputwithlabel/TextInputWithLabel.tsx
@@ -22,11 +22,15 @@ interface Props extends VariantProps<typeof LabelVariants> {
   value: string;
   placeholder?: string;
   maxLength?: number;
-  size?: 'default' | 'withButton';
+  readOnly?: boolean;
+  size?: 'default' | 'withButton' | 'halfButton';
+  inputClassName?: string;
+  inputClassNameCondition?: Record<string, boolean>;
   className?: string;
   classNameCondition?: Record<string, boolean>;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLInputElement>) => void;
   children?: React.ReactNode;
 }
 
@@ -38,15 +42,19 @@ const TextInputWithLabel = ({
   value,
   placeholder,
   maxLength,
+  readOnly,
   size,
+  inputClassName,
+  inputClassNameCondition,
   className,
   classNameCondition,
   onChange,
   onKeyDown,
+  onClick,
   children,
 }: Props) => {
   return (
-    <div className="flex w-fit flex-col gap-1.5">
+    <div className="flex w-fit flex-col">
       <label
         htmlFor={name}
         className={cn(LabelVariants({ state, className }), classNameCondition)}
@@ -60,14 +68,16 @@ const TextInputWithLabel = ({
           value={value}
           maxLength={maxLength}
           placeholder={placeholder}
+          readOnly={readOnly}
           size={size}
-          className={`${className} border-label-normal placeholder-shown:border-line-normal`}
+          className={`${inputClassName} border-label-normal placeholder-shown:border-line-normal`}
           classNameCondition={{
-            ...classNameCondition,
+            ...inputClassNameCondition,
             'peer pl-[38px]': !!children,
           }}
           onChange={onChange}
           onKeyDown={onKeyDown}
+          onClick={onClick}
         />
         <div className="absolute bottom-3.5 left-4 h-[18px] w-[18px] peer-placeholder-shown:text-label-alternative peer-focus:text-label-normal">
           {children}
