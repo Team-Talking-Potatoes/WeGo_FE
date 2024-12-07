04e5e8e023a3020619a87b61409aec9b36e3d986
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-08 02:11:34 +0900

    fix: 유저 아이콘 border 수정

--- src/components/common/user/UserIcon.tsx
+++ src/components/common/user/UserIcon.tsx
@@ -1,18 +1,21 @@
 import Image, { StaticImageData } from 'next/image';
 import { cva, VariantProps } from 'class-variance-authority';
 
-const UserIconVariants = cva('rounded-full overflow-hidden', {
-  variants: {
-    size: {
-      default: 'w-16 h-16',
-      sm: 'w-9 h-9',
-      lg: 'w-20 h-20',
+const UserIconVariants = cva(
+  'rounded-full overflow-hidden border-0.5 border-label-normal border-opacity-15',
+  {
+    variants: {
+      size: {
+        default: 'w-16 h-16',
+        sm: 'w-9 h-9',
+        lg: 'w-20 h-20',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
     },
   },
-  defaultVariants: {
-    size: 'default',
-  },
-});
+);
 
 interface Props extends VariantProps<typeof UserIconVariants> {
   profileImage?: string | StaticImageData;
