f3d96b1d9be50babb35756346c1e61cbf5f09c8f
namgungjongmin <jmnamgung@gmail.com>
2024-12-10 15:38:31 +0900

    feat: 디바이더 컴포넌트 추가
    - 기본 디바이더 컴포넌트
    - 디바이더 위에서 특정 지점을 강조하는 bar의 위치를 결정하는 유틸함수
      추가

---
+++ src/utils/setMypageBarPosition.ts
@@ -0,0 +1,10 @@
+import { SelectedTab } from '@/@types/mypage';
+
+const setMypageBarPosition = (tab: SelectedTab) => {
+  if (tab === 'myTravel') return 'left-0 w-[59px]';
+  if (tab === 'myReview') return 'left-[79px] w-[60px]';
+  if (tab === 'mySelfTravel') return 'left-[159px] w-[91px]';
+  return '';
+};
+
+export default setMypageBarPosition;
