ec74d1b9c6aa4fa6fb876fe6ca0956dc4db8b6b0
namgungjongmin <jmnamgung@gmail.com>
2024-12-13 06:56:07 +0900

    feat: 리뷰 작성가능한 여행 api 적용 및 모킹

--- src/components/mypage/contents/content/myReview/Writable.tsx
+++ src/components/mypage/contents/content/myReview/Writable.tsx
@@ -1,38 +1,36 @@
-import travelListMock from '@/mocks/data/travel/mypage/travelListMock';
 import { useState } from 'react';
 import TravelCard from '@/components/card/TravelCard';
 import Pagenation from '@/components/common/pagenation/Pagenation';
 import { formatStartDate } from '@/utils/dateChageKr';
 import Link from 'next/link';
+import { TravelList } from '@/@types/travel';
+import { useWritableTravel } from '@/queries/travel/useGetMyTravel';
 import NoTravel from '../myTravel/NoTravel';
 
-// 임시 선언 - 나중에 데이터 받아오면 삭제
-const total = 1;
-const travelList = travelListMock.checkedTravel;
-
 const Writable = () => {
-  const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
-  const totalPages = Math.ceil(total / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data: travels } = useWritableTravel(itemsPerPage, currentPage - 1);
+  const totalPages = travels ? Math.ceil(travels.total / itemsPerPage) : 0;
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <section className="mx-auto flex w-[335px] flex-col justify-center gap-6 pb-10">
-      {total > 0 ? (
-        travelList.map((travel) => (
+      {travels && travels.total > 0 ? (
+        travels.travels.map((travel: TravelList) => (
           <div key={travel.travelId} className="relative">
             <TravelCard
               key={travel.travelId}
               travelId={travel.travelId}
               travelName={travel.travelName}
-              maxParticipant={travel.maxParticipant}
+              maxParticipant={travel.maxTravelMateCount}
               isDomestic={travel.isDomestic}
-              travelLocation={travel.travelLocation}
-              currentParticipant={travel.currentParticipant}
+              travelLocation={travel.location}
+              currentParticipant={travel.currentTravelMateCount}
               image={travel.image}
-              startDate={travel.startDate}
-              formattedStartDate={formatStartDate(travel.startDate)}
+              startDate={travel.startAt}
+              formattedStartDate={formatStartDate(travel.startAt)}
               closed
             />
 
