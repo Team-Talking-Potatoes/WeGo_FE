3b02fac84b701792abf7e9ad9eb699872af1a039
namgungjongmin <jmnamgung@gmail.com>
2024-12-13 06:55:39 +0900

    feat: 나의 여행 - 체크한 여행 api 적용 및 모킹

--- src/components/mypage/contents/content/myTravel/CheckedTravel.tsx
+++ src/components/mypage/contents/content/myTravel/CheckedTravel.tsx
@@ -1,36 +1,34 @@
 import TravelCard from '@/components/card/TravelCard';
 import Pagenation from '@/components/common/pagenation/Pagenation';
-import travelListMock from '@/mocks/data/travel/mypage/travelListMock';
 import { formatStartDate } from '@/utils/dateChageKr';
 import { useState } from 'react';
+import { useCheckedTravel } from '@/queries/travel/useGetMyTravel';
+import { TravelList } from '@/@types/travel';
 import NoTravel from './NoTravel';
 
-// 임시 선언 - 나중에 데이터 받아오면 삭제
-const total = 1;
-const travelList = travelListMock.checkedTravel;
-
 const CheckedTravel = () => {
-  const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
-  const totalPages = Math.ceil(total / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data: travels } = useCheckedTravel(itemsPerPage, currentPage - 1);
+  const totalPages = travels ? Math.ceil(travels.total / itemsPerPage) : 0;
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <section className="mx-auto flex w-[335px] flex-col justify-center gap-6 pb-10">
-      {total > 0 ? (
-        travelList.map((travel) => (
+      {travels && travels.total > 0 ? (
+        travels.travels.map((travel: TravelList) => (
           <TravelCard
             key={travel.travelId}
             travelId={travel.travelId}
             travelName={travel.travelName}
-            maxParticipant={travel.maxParticipant}
+            maxParticipant={travel.maxTravelMateCount}
             isDomestic={travel.isDomestic}
-            travelLocation={travel.travelLocation}
+            travelLocation={travel.location}
             image={travel.image}
-            currentParticipant={travel.currentParticipant}
-            startDate={travel.startDate}
-            formattedStartDate={formatStartDate(travel.startDate)}
+            currentParticipant={travel.currentTravelMateCount}
+            startDate={travel.startAt}
+            formattedStartDate={formatStartDate(travel.startAt)}
             checkMark
             isChecked
           />
