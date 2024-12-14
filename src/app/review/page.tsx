8fe01ce45dbe8ee3544f568179a742ae57cd4255
namgungjongmin <jmnamgung@gmail.com>
2024-12-14 22:42:50 +0900

    feat: review 페이지 생성 및 header 적용

---
+++ src/app/review/page.tsx
@@ -0,0 +1,11 @@
+import ReviewHeader from '@/components/review/reviewHeader/ReviewHeader';
+
+const ReviewPage = () => {
+  return (
+    <div className="flex flex-col px-5 pb-[75px] pt-[60px]">
+      <ReviewHeader />
+    </div>
+  );
+};
+
+export default ReviewPage;
---
+++ src/components/review/reviewHeader/ReviewHeader.tsx
@@ -0,0 +1,15 @@
+import ButtonRounded from '@/components/common/button/ButtonRounded';
+import Link from 'next/link';
+
+const ReviewHeader = () => {
+  return (
+    <header className="flex items-center justify-between pb-6 pt-9">
+      <div className="title-2-b text-label-normal">여행 리뷰</div>
+      <Link href="/travel/new" aria-label="여행 만들기 버튼">
+        <ButtonRounded label="리뷰작성" />
+      </Link>
+    </header>
+  );
+};
+
+export default ReviewHeader;
