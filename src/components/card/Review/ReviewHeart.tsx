12e1e368c5f6f3668d97abb691cab906701f809e
namgungjongmin <jmnamgung@gmail.com>
2024-12-27 17:58:40 +0900

    design: 리뷰 카드 및 좋아요 버튼 애니메이션 추가

--- src/components/card/Review/ReviewHeart.tsx
+++ src/components/card/Review/ReviewHeart.tsx
@@ -1,35 +1,32 @@
 import HeartIcon from '@/assets/icon/review/heart_18px.svg';
 import cn from '@/utils/cn';
-import { useState } from 'react';
 
 interface Props {
-  isLiked: boolean;
+  isLiked?: boolean;
+  animate: boolean;
+  handler: (e: React.MouseEvent<HTMLButtonElement>) => void;
   red?: boolean;
 }
 
-const ReviewHeart = ({ isLiked, red }: Props) => {
+const ReviewHeart = ({ isLiked, animate, handler, red }: Props) => {
   // 백엔드쪽 좋아요 기능 구현 후 대체 예정
-  const [isLikedState, setIsLikedState] = useState(isLiked);
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setIsLikedState(!isLikedState);
-  };
   // --------------------------
 
   return (
     <button
       type="button"
-      onClick={handleClick}
+      onClick={handler}
       className="absolute right-2 top-2 z-50 flex h-9 w-9 items-center justify-center rounded bg-black/30"
     >
       <HeartIcon
         data-testid="heart-icon"
-        className={cn('fill-none', {
+        className={cn('forwards fill-none transition-all duration-100', {
           'stroke-primary-white': !red,
-          'fill-primary-white': !red && isLikedState,
+          'fill-primary-white': !red && isLiked,
           'stroke-red-400': red,
-          'fill-red-400': red && isLikedState,
+          'fill-red-400': red && isLiked,
+          'animate-check-shake': animate && isLiked,
+          'animate-check-shake-reverse': animate && !isLiked,
         })}
       />
     </button>
