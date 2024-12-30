db810bb5d3958b14aefc15bfc89f12f102ae8874
namgungjongmin <jmnamgung@gmail.com>
2024-12-30 09:29:53 +0900

    test: 디자인 수정에 따른 테스트 점검

--- src/components/card/Review/ReviewHeart.test.tsx
+++ src/components/card/Review/ReviewHeart.test.tsx
@@ -1,24 +1,22 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ReviewHeart from './ReviewHeart';
 
 describe('ReviewHeart 컴포넌트 테스트', () => {
   it('기본 렌더링이 올바른지 확인', () => {
-    render(<ReviewHeart isLiked={false} />);
+    render(<ReviewHeart isLiked={false} animate={false} handler={() => {}} />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
     expect(screen.getByTestId('heart-icon')).toHaveClass('fill-none');
   });
 
-  it('버튼 클릭 시 상태가 변경되는지 확인', () => {
-    render(<ReviewHeart isLiked={false} />);
-    const button = screen.getByRole('button');
-    fireEvent.click(button);
+  it('isLiked이 true일 때 클래스가 올바른지 확인', () => {
+    render(<ReviewHeart isLiked animate={false} handler={() => {}} />);
     expect(screen.getByTestId('heart-icon')).toHaveClass('fill-primary-white');
   });
 
   it('red prop이 true일 때 클래스가 올바른지 확인', () => {
-    render(<ReviewHeart isLiked red />);
+    render(<ReviewHeart isLiked red animate={false} handler={() => {}} />);
     expect(screen.getByTestId('heart-icon')).toHaveClass('fill-red-400');
   });
 });
