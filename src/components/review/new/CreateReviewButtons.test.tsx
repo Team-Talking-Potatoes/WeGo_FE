5109e57456c841ae5bb043fbe52b5c9f37614fe1
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-12-30 10:02:36 +0900

    test: 테스트 코드 수정

--- src/components/review/new/CreateReviewButtons.test.tsx
+++ src/components/review/new/CreateReviewButtons.test.tsx
@@ -1,46 +1,89 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { useRouter } from 'next/navigation';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useCreateReview } from '@/queries/review/useCreateReview';
+
+import { create } from '@/mocks/zustand';
 import CreateReviewButtons from './CreateReviewButtons';
+
+type CreateReviewStore = {
+  resetStore: jest.Mock;
+};
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
 }));
 
-describe('CreateReviewButtons', () => {
-  it('리뷰 생성, 취소 버튼을 렌더링합니다', () => {
-    const mockBack = jest.fn();
-    (useRouter as jest.Mock).mockReturnValue({ back: mockBack });
-    const queryClient = new QueryClient();
+const mockResetStore = jest.fn();
+const useCreateReviewStore = create<CreateReviewStore>(() => ({
+  resetStore: mockResetStore,
+}));
 
+jest.mock('@/queries/review/useCreateReview', () => ({
+  useCreateReview: jest.fn(() => ({
+    handleSubmit: jest.fn(),
+  })),
+}));
+
+describe('CreateReviewButtons', () => {
+  const mockRouter = { back: jest.fn() };
+  const mockClickNext = jest.fn();
+
+  const mockHandleSubmit = jest.fn();
+
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    (useCreateReview as jest.Mock).mockReturnValue({
+      handleSubmit: mockHandleSubmit,
+    });
+    jest.clearAllMocks();
+    act(() => {
+      useCreateReviewStore.setState({ resetStore: mockResetStore });
+    });
+  });
+
+  it('리뷰 생성 페이지에서 첫 페이지일 경우 "다음" 버튼을 렌더링합니다', () => {
+    render(<CreateReviewButtons isFirstPage clickNext={mockClickNext} />);
+
+    expect(screen.getByText('취소')).toBeInTheDocument();
+    expect(screen.getByText('다음')).toBeInTheDocument();
+  });
+
+  it('리뷰 생성 페이지에서 첫 페이지가 아닐 경우 "확인" 버튼을 렌더링합니다', () => {
     render(
-      <QueryClientProvider client={queryClient}>
-        <CreateReviewButtons isFirstPage clickNext={jest.fn()} />
-      </QueryClientProvider>,
+      <CreateReviewButtons isFirstPage={false} clickNext={mockClickNext} />,
     );
 
-    const cancleButton = screen.getByText('취소');
-    expect(cancleButton).toBeInTheDocument();
-    fireEvent.click(cancleButton);
-    expect(mockBack).toHaveBeenCalledTimes(1);
-
+    expect(screen.getByText('취소')).toBeInTheDocument();
     expect(screen.getByText('확인')).toBeInTheDocument();
   });
 
-  it('필수 입력값이 없으면 submit되지 않습니다', () => {
-    const queryClient = new QueryClient();
+  it('취소 버튼을 클릭하면 resetStore가 호출되고, router.back이 호출됩니다', () => {
+    render(<CreateReviewButtons isFirstPage clickNext={mockClickNext} />);
+
+    const cancelButton = screen.getByText('취소');
+    fireEvent.click(cancelButton);
+
+    expect(mockRouter.back).toHaveBeenCalledTimes(1);
+  });
+
+  it('다음 버튼을 클릭하면 clickNext가 호출됩니다', () => {
+    render(<CreateReviewButtons isFirstPage clickNext={mockClickNext} />);
+
+    const nextButton = screen.getByText('다음');
+    fireEvent.click(nextButton);
+
+    expect(mockClickNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('확인 버튼을 클릭하면 handleSubmit이 호출됩니다', () => {
     render(
-      <QueryClientProvider client={queryClient}>
-        <CreateReviewButtons isFirstPage clickNext={jest.fn()} />
-      </QueryClientProvider>,
+      <CreateReviewButtons isFirstPage={false} clickNext={mockClickNext} />,
     );
 
-    // 버튼 클릭 전 값이 비어있으면 handleSubmit이 호출되지 않아야 함
     const confirmButton = screen.getByText('확인');
     fireEvent.click(confirmButton);
-    expect(
-      screen.queryByText('리뷰 생성 중 오류 발생:'),
-    ).not.toBeInTheDocument();
+
+    expect(mockHandleSubmit).toHaveBeenCalledTimes(1);
   });
 });
