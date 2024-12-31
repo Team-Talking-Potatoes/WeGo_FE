8d9853fe387b0f7521547579f83e02c86171d69a
namgungjongmin <jmnamgung@gmail.com>
2024-12-31 10:14:13 +0900

    fix: useSearchParams() 영향받는 컴포넌트 suspense

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,14 +1,17 @@
 import ReviewList from '@/components/review/reviewList/ReviewList';
 import ReviewHeader from '@/components/review/reviewList/reviewHeader/ReviewHeader';
+import { Suspense } from 'react';
 
 const ReviewPage = () => {
   return (
-    <div className="flex justify-center px-5 pb-[104px] pt-[60px]">
-      <div className="flex w-full max-w-[335px] flex-col md:max-w-[688px] xl:max-w-[1400px]">
-        <ReviewHeader />
-        <ReviewList />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex justify-center px-5 pb-[104px] pt-[60px]">
+        <div className="flex w-full max-w-[335px] flex-col md:max-w-[688px] xl:max-w-[1400px]">
+          <ReviewHeader />
+          <ReviewList />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
