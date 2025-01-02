91578263ef95d465509fba09df37fff69461f3bc
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-02 16:20:15 +0900

    fix: props 설정 수정

--- src/utils/dateChangeKr.ts
+++ src/utils/dateChangeKr.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 
 // 현재 날짜를 기준으로 '내일'을 계산
 export const checkTomorrow = (checkDate: string): string => {
@@ -22,7 +23,7 @@ export const getWeekNumber = (dateFrom = dayjs()) => {
 };
 
 /**
- * @param dateString 변환하려는 날짜(2024.12.03)
+ * @param dateString 변환하려는 날짜(20241203)
  * @param offsetDays dateString을 기준으로 더하고 싶은 숫자
  * @param includeYear 연도 포함 여부
  * @return 2024.12.03(화) 혹은 12.03(화)
@@ -30,19 +31,16 @@ export const getWeekNumber = (dateFrom = dayjs()) => {
 export const formatDateToShortWithDay = (
   dateString: string,
   offsetDays?: number,
-  includeYear?: boolean,
+  includeYear: boolean = true,
 ): string => {
-  const date = new Date(dateString);
+  let date = dayjs(dateString, 'YYYYMMDD').locale('ko');
+
   if (offsetDays) {
-    date.setDate(date.getDate() + offsetDays);
+    date = date.add(offsetDays, 'day');
   }
-  const option: Intl.DateTimeFormatOptions = { weekday: 'short' };
-  const dayName = new Intl.DateTimeFormat('ko-KR', option).format(date);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const year = date.getFullYear();
+  const dayName = date.format('ddd');
 
   return includeYear
-    ? `${year}.${month}.${day}(${dayName})`
-    : `${month}.${day}(${dayName})`;
+    ? date.format(`YYYY.MM.DD(${dayName})`)
+    : date.format(`MM.DD(${dayName})`);
 };
