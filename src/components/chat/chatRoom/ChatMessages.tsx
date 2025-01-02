bbfd4bc32de9ae887b40781bf238c2e8532ed6e5
who0803 <10rlatkdtn@naver.com>
2025-01-02 13:30:20 +0900

    feat: 채팅방 컴포넌트 반응형 구현

--- src/components/chat/chatRoom/ChatMessages.tsx
+++ src/components/chat/chatRoom/ChatMessages.tsx
@@ -1,14 +1,26 @@
+/* eslint-disable no-param-reassign */
 import { useEffect, useRef, useState } from 'react';
 import Chat from '@/assets/chat_gray.svg';
-import { Message } from '@/@types/chat';
+import { ChatMessage as ChatMessageType } from '@/@types/chat';
 import ChatMessage from '@/components/chat/chatRoom/ChatMessage';
 
 interface Props {
-  messages: Message[];
+  isFetchingPreviousRef: React.MutableRefObject<boolean>;
+  messages: ChatMessageType[];
   textareaHeight: number;
+  children: React.ReactNode;
+  messagesContainerRef: React.RefObject<HTMLUListElement>;
+  nickname: string;
 }
 
-const ChatMessages = ({ messages, textareaHeight }: Props) => {
+const ChatMessages = ({
+  isFetchingPreviousRef,
+  messages,
+  textareaHeight,
+  children,
+  messagesContainerRef,
+  nickname,
+}: Props) => {
   const [isError] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
   const messageRefs = useRef<Record<string, HTMLDivElement | null>>({});
@@ -43,42 +55,62 @@ const ChatMessages = ({ messages, textareaHeight }: Props) => {
   }, [messages]);
 
   useEffect(() => {
-    scrollToBottom();
-  }, [messages, textareaHeight]);
+    if (!isFetchingPreviousRef.current) {
+      requestAnimationFrame(() => {
+        scrollToBottom();
+      });
+    }
+    isFetchingPreviousRef.current = false;
+  }, [messages, textareaHeight, isFetchingPreviousRef]);
+
+  const isMdScreen = window.matchMedia('(min-width: 768px)').matches;
+  const height = isMdScreen
+    ? `calc(100vh - ${textareaHeight + 190}px)`
+    : `calc(100vh - ${textareaHeight + 110}px)`;
+
+  if (messages.length === 0) {
+    return (
+      <div className="mt-[60px] flex h-[calc(100vh-140px)] flex-col items-center justify-center md:mt-[180px] md:justify-start xl:mt-[300px]">
+        <Chat className="mb-4" />
+        <p className="body-1-m text-center text-label-alternative">
+          채팅이 시작 되었습니다!
+          <br />
+          여행에 대해 활발하게 의견을 나눠보세요 :)
+        </p>
+      </div>
+    );
+  }
 
   return (
     <ul
-      className="overflow-y-auto p-5 custom-scrollbar"
+      ref={messagesContainerRef}
+      className="mt-[60px] overflow-y-auto p-5 custom-scrollbar"
       style={{
-        height: `calc(100vh - ${110 + textareaHeight}px)`,
+        height,
       }}
     >
-      {messages.length === 0 ? (
-        <div className="absolute inset-0 m-auto flex h-fit flex-col items-center">
-          <Chat className="mb-4" />
-          <p className="body-1-m text-center text-label-alternative">
-            채팅이 시작 되었습니다!
-            <br />
-            여행에 대해 활발하게 의견을 나눠보세요 :)
-          </p>
-        </div>
-      ) : (
-        messages.map((msg, index) => (
-          <li key={msg.id}>
-            <ChatMessage
-              messages={messages}
-              message={msg}
-              messageIndex={index}
-              isError={isError}
-              textBoxHeights={textBoxHeights}
-              messageRefs={messageRefs}
-            />
-          </li>
-        ))
-      )}
+      {children}
+      {messages.map((msg, index) => (
+        <li key={msg.chatMessageId}>
+          <ChatMessage
+            messages={messages}
+            message={msg}
+            messageIndex={index}
+            isError={isError}
+            textBoxHeights={textBoxHeights}
+            messageRefs={messageRefs}
+            nickname={nickname}
+          />
+        </li>
+      ))}
       <div ref={messagesEndRef} />
     </ul>
   );
 };
 
 export default ChatMessages;
+
+/**
+ 
+
+ */
