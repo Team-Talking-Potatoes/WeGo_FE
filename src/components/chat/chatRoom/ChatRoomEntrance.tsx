a4613e78e4972c497448611a4393fedfea41a802
who0803 <10rlatkdtn@naver.com>
2025-01-02 13:18:27 +0900

    feat: 채팅방 참여 컴포넌트 반응형 구현

--- src/components/chat/chatRoom/ChatRoomEntrance.tsx
+++ src/components/chat/chatRoom/ChatRoomEntrance.tsx
@@ -1,53 +1,69 @@
 import Header from '@/components/common/header/Header';
 import Image from 'next/image';
-import { JoinedData } from '@/@types/chat';
+import { RoomResponse } from '@/@types/chat';
+import { Button } from '@/components/common/button/Button';
+import { useSetIsJoined } from '@/queries/chat/useSetChat';
+import UserIcon from '@/components/common/user/UserIcon';
 
 interface Props {
-  chatData: JoinedData;
-  children: React.ReactNode;
+  chatId: string;
+  chatRoomData: RoomResponse;
+  onCloseChatRoom: () => void;
 }
 
-const ChatRoomEntrance = ({ chatData, children }: Props) => {
+const ChatRoomEntrance = ({ chatId, chatRoomData, onCloseChatRoom }: Props) => {
+  const { mutate } = useSetIsJoined();
+
+  const handleJoinChat = () => {
+    mutate({ chatId });
+  };
+
   const {
     image,
-    title,
+    chattingName,
     description,
     host,
-    hostImage,
+    hostProfileImage,
     totalMembersCount,
     membersCount,
-  } = chatData;
+  } = chatRoomData;
+
   return (
     <>
-      <Header title="채팅" />
-      <div className="mb-[120px] mt-10 flex h-full flex-1 flex-col items-center text-center">
+      <Header onRoute={onCloseChatRoom} title="채팅" isChatHeader />
+
+      <div className="mb-[120px] mt-[100px] flex h-full flex-1 flex-col items-center text-center xl:mt-[calc(60px+18%)]">
         <div className="relative mb-6 h-[100px] w-[100px] overflow-hidden rounded-full">
           <Image
-            src={image as string}
-            alt={title as string}
+            src={image}
+            alt={chattingName}
             layout="fill"
             objectFit="cover"
           />
         </div>
-        <h2 className="title-4-b mb-1 text-label-normal">{title}</h2>
+        <h2 className="title-4-b mb-1 text-label-normal">{chattingName}</h2>
         <p className="body-2-r mb-6 text-label-alternative">{description}</p>
         <div className="flex">
-          <div className="relative mr-[7px] h-[24px] w-[24px] overflow-hidden rounded-full">
-            <Image
-              src={hostImage as string}
-              alt={host as string}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <span className="body-2-b text-label-neutral after:mx-2 after:text-line-normal after:content-['|']">
+          <UserIcon
+            size="xs"
+            profileImage={hostProfileImage}
+            nickname={host}
+            ariaLabel={`${host ?? '유저'}의 프로필 이미지`}
+          />
+          <span className="body-2-b ml-[7px] text-label-neutral after:mx-2 after:text-line-normal after:content-['|']">
             {host}
           </span>
           <span className="body-2-m text-label-alternative">
             {membersCount}/{totalMembersCount}명
           </span>
         </div>
-        {children}
+        <Button
+          handler={handleJoinChat}
+          fill="blue"
+          className="mt-auto xl:mt-[120px]"
+        >
+          채팅방 참여하기
+        </Button>
       </div>
     </>
   );
