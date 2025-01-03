87b29f13297caeda5fdc0f8bea370d1375f3c563
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-03 18:37:07 +0900

    test: 빌드 테스트

--- src/components/main/weeklyTravel/WeeklyPopularContainer.tsx
+++ src/components/main/weeklyTravel/WeeklyPopularContainer.tsx
@@ -9,14 +9,21 @@ import WeeklyPopular from './WeeklyPopular';
 
 const WeeklyPopularContainer = async () => {
   const queryClient = new QueryClient();
-  const data = await queryClient.fetchQuery({
-    queryKey: QUERY_KEYS.TRAVEL.POPULAR_TRAVEL,
-    queryFn: getPopularTravel,
-  });
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <WeeklyPopular travelList={data.data} />
-    </HydrationBoundary>
-  );
+
+  try {
+    const data = await queryClient.fetchQuery({
+      queryKey: QUERY_KEYS.TRAVEL.POPULAR_TRAVEL,
+      queryFn: getPopularTravel,
+      staleTime: 60 * 1000,
+    });
+
+    return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <WeeklyPopular travelList={data.data} />
+      </HydrationBoundary>
+    );
+  } finally {
+    queryClient.clear();
+  }
 };
 export default WeeklyPopularContainer;
