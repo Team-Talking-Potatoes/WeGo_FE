d1aa84fd83978fc34b7860cf6cf6b003b41afdbe
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-05 02:47:46 +0900

    fix: 여행 상세 버튼 기능 분류 - 북마크 api 수정 - 버튼 컴포넌트 분리

---
+++ src/components/travel/detail/category/TravelDetailButtons.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import SpinnerButtonIcon from '@/assets/icon/loading/spinner-button.svg';
+import BookMarkIcon from '@/assets/bookmark.svg';
+import {
+  useBookmarkTravel,
+  useDeleteBookmarkTravel,
+} from '@/queries/travel/useBookmarkTravel';
+import ButtonRounded from '@/components/common/button/ButtonRounded';
+import Link from 'next/link';
+
+const TravelDetailButtons = ({
+  organizerId,
+  userId,
+  isBookMark,
+  travelId,
+}: {
+  organizerId: number;
+  userId: number;
+  isBookMark: boolean;
+  travelId: number;
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isBookmarked, setIsBookmarked] = useState(isBookMark);
+  const { mutate: postBookMark } = useBookmarkTravel();
+  const { mutate: deleteBookMark } = useDeleteBookmarkTravel();
+
+  useEffect(() => {
+    setIsBookmarked(isBookMark);
+    setIsLoading(false);
+  }, [isBookMark]);
+
+  const handleClickBookMark = () => {
+    if (isBookmarked) {
+      deleteBookMark(travelId, {
+        onError: () => setIsBookmarked(true),
+      });
+    } else {
+      postBookMark(travelId, {
+        onError: () => setIsBookmarked(false),
+      });
+    }
+    setIsBookmarked((prev) => !prev);
+  };
+
+  if (isLoading) {
+    return <SpinnerButtonIcon className="animate-spin" />;
+  }
+
+  return (
+    <div className="flex items-center gap-2.5">
+      {organizerId !== userId ? (
+        <button onClick={handleClickBookMark} type="button" aria-label="북마크">
+          <BookMarkIcon
+            fill={isBookmarked ? '#F87171' : 'white'}
+            className={
+              isBookmarked
+                ? 'animate-check-shake'
+                : 'animate-check-shake-reverse'
+            }
+          />
+        </button>
+      ) : (
+        <Link href="/">
+          <ButtonRounded label="일정수정" color="blue" />
+        </Link>
+      )}
+      <Link href="/chat">
+        <ButtonRounded label="채팅방" />
+      </Link>
+    </div>
+  );
+};
+
+export default TravelDetailButtons;
