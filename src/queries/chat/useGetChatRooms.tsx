c12117383e930d361aa13da3a316982b4b05ba58
who0803 <10rlatkdtn@naver.com>
2025-01-05 19:14:38 +0900

    feat: 리액트 쿼리 리패칭 방지

--- src/queries/chat/useGetChatRooms.tsx
+++ src/queries/chat/useGetChatRooms.tsx
@@ -9,6 +9,12 @@ const useGetChatRooms = (sortBy: SortType) => {
       const [, sort] = queryKey as [string, SortType];
       return getChatRooms(sort);
     },
+    staleTime: Infinity,
+    gcTime: Infinity,
+    refetchInterval: Infinity,
+    refetchOnWindowFocus: false, // 윈도우 포커스 시 리패칭 방지
+    refetchOnReconnect: false, // 네트워크 복구 시 리패칭 방지
+    refetchOnMount: false, // 컴포넌트 마운트 시 리패칭 방지
   });
 };
 
