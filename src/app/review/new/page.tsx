9bd6e31869b2476845a1a512cf735ced83fa0541
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-05 22:39:32 +0900

    fix: 파라미터 수정, 로딩바 생성

--- src/app/review/new/page.tsx
+++ src/app/review/new/page.tsx
@@ -5,16 +5,24 @@ import CreateReviewButtons from '@/components/review/new/CreateReviewButtons';
 import SelectTravel from '@/components/review/new/SelectTravel';
 import SelectHashTag from '@/components/review/new/organizer/SelectHashTag';
 import ReviewParticipantContainer from '@/components/review/new/participant/ReviewParticipantContainer';
-import { Suspense, use, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import SpinnerIcon from '@/assets/icon/loading/spinner-button.svg';
 
-const CreateReviewPage = ({
-  searchParams,
-}: {
-  searchParams: Promise<{ id?: string; title?: string }>;
-}) => {
-  const { id, title } = use(searchParams);
+const CreateReviewPage = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
+  const title = searchParams.get('title') || undefined;
   const travelId = id ? Number(id) : undefined;
   const [isFirstPage, setIsFirstPage] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (travelId) {
+      setIsLoading(false);
+    }
+  }, [travelId]);
+
   return (
     <form
       method="dialog"
@@ -22,10 +30,15 @@ const CreateReviewPage = ({
     >
       <CreateReviewHeader />
       <main className="flex flex-col gap-6">
-        <Suspense fallback={<div>Loading...</div>}>
+        {isLoading ? (
+          <div className="mt-4 flex h-[46px] items-center justify-center rounded border border-line-normal">
+            <SpinnerIcon className="animate-spin" />
+          </div>
+        ) : (
           <SelectTravel id={travelId} title={title} />
-          {isFirstPage ? <SelectHashTag /> : <ReviewParticipantContainer />}
-        </Suspense>
+        )}
+
+        {isFirstPage ? <SelectHashTag /> : <ReviewParticipantContainer />}
       </main>
 
       <CreateReviewButtons
