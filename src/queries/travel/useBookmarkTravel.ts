dd7687cd53d60bc354d05306d17cd83662d9dcda
namgungjongmin <jmnamgung@gmail.com>
2025-01-05 22:41:18 +0900

    refactor: 북마크 캐싱 로직 추가

--- src/queries/travel/useBookmarkTravel.ts
+++ src/queries/travel/useBookmarkTravel.ts
@@ -1,20 +1,30 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { QueryError } from '@/@types/query';
 import { deleteTravelBookMark, postTravelBookMark } from '@/api/travel/travels';
 import { useQueryErrorHandler } from '../common/errorHandler';
 
 export const useBookmarkTravel = () => {
+  const queryClient = useQueryClient();
   const handleError = useQueryErrorHandler();
+
   return useMutation({
     mutationFn: postTravelBookMark,
     onError: (error: QueryError) => handleError(error),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['checkedTravel'] });
+    },
   });
 };
 
 export const useDeleteBookmarkTravel = () => {
+  const queryClient = useQueryClient();
   const handleError = useQueryErrorHandler();
+
   return useMutation({
     mutationFn: deleteTravelBookMark,
     onError: (error: QueryError) => handleError(error),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['checkedTravel'] });
+    },
   });
 };
