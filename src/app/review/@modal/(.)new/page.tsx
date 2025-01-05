9e5bcc978016120623e8cdd30305cdd924b99f7d
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2025-01-05 22:43:53 +0900

    fix: 코드 수정

--- src/app/review/@modal/(.)new/page.tsx
+++ src/app/review/@modal/(.)new/page.tsx
@@ -1,10 +1,13 @@
 import ReviewModal from '@/components/review/new/ReviewModal';
+import { Suspense } from 'react';
 import WriteReviewPage from '../../new/page';
 
 const CreateReview = () => {
   return (
     <ReviewModal>
-      <WriteReviewPage />
+      <Suspense fallback={<div>로딩중...</div>}>
+        <WriteReviewPage />
+      </Suspense>
     </ReviewModal>
   );
 };
