bf592d0f7768b5e350bf559030a890f9c6869e75
namgungjongmin <jmnamgung@gmail.com>
2025-01-24 17:06:19 +0900

    refactor: export 방식 변경

--- src/@types/auth.ts
+++ src/@types/auth.ts
@@ -1,4 +1,4 @@
-interface TextInput {
+export interface TextInput {
   email: string;
   verifyNumber: number;
   name: string;
@@ -7,17 +7,15 @@ interface TextInput {
   contact: string;
 }
 
-interface PasswordInput {
+export interface PasswordInput {
   currentPassword: string;
   password: string;
   newPassword: string;
   passwordConfirm: string;
 }
 
-interface ValidateOptions {
+export interface ValidateOptions {
   name: keyof TextInput | keyof PasswordInput;
   value: string;
   password?: string;
 }
-
-export type { TextInput, PasswordInput, ValidateOptions };
