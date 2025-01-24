f91c727bbab9f04f838d8a8df9dfa3d14e3f8fbe
namgungjongmin <jmnamgung@gmail.com>
2025-01-24 16:53:49 +0900

    refactor: 인증 관련 엔드포인트 변경

--- src/api/auth/verifyEmail.ts
+++ src/api/auth/verifyEmail.ts
@@ -1,4 +1,5 @@
 import { TextInput } from '@/@types/auth';
+import { BaseResponse } from '@/@types/api';
 import { http } from '../fetcher';
 
 type SendMailRequestBody = Pick<TextInput, 'email'>;
@@ -6,17 +7,17 @@ type CheckCodeRequestBody = Pick<TextInput, 'email' | 'verifyNumber'>;
 
 /* -------------------------------- 인증 이메일 전송 (회원가입)------------------------------- */
 export const sendMail = (credentials: SendMailRequestBody) => {
-  return http.post<any>('/auth/sign-up/emails', credentials);
+  return http.post<BaseResponse<object>>('/auth/emails/sign-up', credentials);
 };
 
 /* -------------------------------- 인증 이메일 전송 (비밀번호 재설정)------------------------------- */
 export const passwordSendMail = (credentials: SendMailRequestBody) => {
-  return http.post<any>('/auth/password/emails', credentials);
+  return http.post<BaseResponse<object>>('/auth/emails/password', credentials);
 };
 
 /* ------------------------------- 이메일 인증코드 확인 ------------------------------ */
 export const checkCode = ({ email, verifyNumber }: CheckCodeRequestBody) => {
-  return http.post<any>('/auth/emails/verify', {
+  return http.post<BaseResponse<object>>('/auth/emails/verification', {
     email,
     verifyNumber,
   });
