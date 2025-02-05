fd26f4c1988a849c92d3b27c031c309ab0e0f8c9
namgungjongmin <jmnamgung@gmail.com>
2025-02-05 09:27:24 +0900

    refactor: 리뷰 관련 타입 수정

--- src/@types/review.ts
+++ src/@types/review.ts
@@ -2,36 +2,37 @@ export interface Review {
   // 리뷰 정보
   reviewId: number;
   reviewImage: string;
-  title?: string;
-  content?: string;
-  comment?: string;
-  reviewImages?: string[];
-  starRating?: number;
-  likesCount?: number;
-  likesFlag?: boolean;
-  createdAt?: string;
-  travelLocation?: string;
+  title: string;
+  content: string;
+  comment: string;
+  reviewImages: string[];
+  starRating: number;
+  likesCount: number;
+  likesFlag: boolean;
+  createdAt: string;
+  travelLocation: string;
 
   // 여행 정보
-  travelId?: number;
-  travelTitle?: string;
+  travelId: number;
+  travelTitle: string;
 
   // 유저 정보
-  nickname?: string;
-  profileImage?: string;
+  nickname: string;
+  profileImage: string;
 }
 
-export interface ReviewForm {
-  travelId: number;
-  title: string;
-  comment: string;
-  starRating: number;
-  organizerReviewTags: string;
-  images: string[];
+export interface ReviewRatings {
+  totalReviews: number;
+  oneStarReviews: number;
+  twoStarReviews: number;
+  threeStarReviews: number;
+  fourStarReviews: number;
+  fiveStarReviews: number;
 }
 
-export interface Filters {
-  sortOrder: 'createdAt' | 'popular';
+export interface ReviewScore {
+  totalRating: number;
+  reviewRatings: ReviewRatings;
 }
 
 export interface ReviewListFilters {
